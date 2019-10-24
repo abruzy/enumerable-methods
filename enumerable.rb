7ee7a8c8200464b47c511d1084aea5dbbde8cec9
abruzy <dialloabubakar5861@gmail.com>
2019-10-24 09:50:38 +0100

    fix: refactor my_any, my_all, my_none, my_map, and my_inject

--- enumerable.rb
+++ enumerable.rb
@@ -31,60 +31,69 @@ module Enumerable
     new_arr
   end
 
-  def my_all?(pattern = nil)
-    return my_all? { |e| pattern.match(e) } unless pattern.nil?
-
-    return my_all? { |e| e } unless block_given?
-
-    my_each { |e| return false unless yield(e) }
-    true
+  def my_all?(classification = nil)
+    test = true
+    checked = classification.class
+    my_each { |item| return false if yield(item) == false } if block_given?
+    my_each { |item| return false unless item.class == classification } if checked == Class
+    my_each { |item| return false unless item =~ classification } if checked == Regexp
+    my_each { |item| return false unless item == classification } if [Integer, String].include?(checked)
+    my_each { |item| return false unless item } if !classification && !block_given?
+    test
   end
 
-  def my_any?(pattern = nil)
-    return my_any? { |e| pattern.match(e) } unless pattern.nil?
-
-    return my_any? { |e| e } unless block_given?
-
-    my_each { |e| return true if yield(e) }
-    false
+  def my_any?(classification = nil)
+    test = false
+    my_each { |item| return true if yield(item) == true } if block_given?
+    my_each { |item| return true if item.class == classification } if classification.class == Class
+    my_each { |item| return true if item =~ classification } if classification.class == Regexp
+    my_each { |item| return true if item == classification } if [Integer, String].include?(classification.class)
+    my_each { |item| return true if item } if !classification && !block_given?
+    test
   end
 
-  def my_none?(*pattern)
-    return !my_any?(*pattern) unless block_given?
-
-    !my_any? { |e| yield(e) }
+  def my_none?(classification = nil)
+    test = true
+    my_each { |item| return false if yield(item) == true } if block_given?
+    my_each { |item| return false if item.class == classification } if classification.class == Class
+    my_each { |item| return false if item =~ classification } if classification.class == Regexp
+    my_each { |item| return false if item == classification } if [Integer, String].include?(classification.class)
+    my_each { |item| return false if item } if !classification && !block_given?
+    test
   end
 
-  def my_count
+  def my_count(classification = nil)
+    counter = 0
+    my_each { |item| counter += 1 if item == classification } if [Integer, String].include?(classification.class)
+    my_each { |item| counter += 1 if yield(item) == true } if block_given?
+    my_each { |item| counter += 1 if item.class == classification } if classification.class == Class
+    return length if !classification && !block_given?
+
+    counter
+  end
+
+  def my_map(&my_proc)
+    return to_enum(:my_map) unless block_given?
+
     i = 0
-    count = []
-    if block_given?
-      while i < size
-        count << self[i] if yield(self[i])
-        i += 1
-      end
-      return count.size
-    end
-    size
-  end
-
-  def my_map(&block)
-    arr = []
-    my_each do |e|
-      arr << block.call(e)
-    end
-    arr
-  end
-
-  def my_inject(initial = nil)
-    result = initial.nil? ? self[0] : initial
-    (1..length - 1).each do |i|
-      result = yield(result, self[i])
+    result = []
+    while i < length
+      answer = my_proc.call(self[i])
+      result << answer
+      i += 1
     end
     result
   end
-end
 
-def multiply_els(arr)
-  arr.my_inject { |x, y| x * y }
+  def my_inject(total = nil, current = nil)
+    arr = is_a?(Range) ? to_a : self
+    beginner = total.nil? || total.is_a?(Symbol) ? arr[0] : total
+    if block_given?
+      start = total ? 0 : 1
+      arr[start..-1].my_each { |item| beginner = yield(beginner, item) }
+    end
+    arr[1..-1].my_each { |item| beginner = beginner.send(total, item) } if total.is_a?(Symbol)
+    arr[0..-1].my_each { |item| beginner = beginner.send(current, item) } if current
+    beginner
+  end
 end
